545c23b7dd9925807a774ed5bb0ed0fbf9d3c918
Yu Matsuzawa <ymtszw@gmail.com>
2021-05-24 04:12:59 +0900

    feat: throttle file change events before dispatching assets_change

--- lib/phoenix_live_reload/channel.ex
+++ lib/phoenix_live_reload/channel.ex
@@ -1,3 +1,7 @@
+#
+# Patch this file to enable HMR; Hot Module Reloading for other asset types
+#
+
 defmodule Phoenix.LiveReloader.Channel do
   @moduledoc """
   Phoenix's live-reload channel.
@@ -5,38 +9,48 @@ defmodule Phoenix.LiveReloader.Channel do
   use Phoenix.Channel
   require Logger
 
+  @impl true
   def join("phoenix:live_reload", _msg, socket) do
     {:ok, _} = Application.ensure_all_started(:phoenix_live_reload)
+    patterns = socket.endpoint.config(:live_reload)[:patterns]
 
     if Process.whereis(:phoenix_live_reload_file_monitor) do
+      Logger.debug("Browser connected to live reload! Endpoint: " <> inspect(socket.endpoint))
       FileSystem.subscribe(:phoenix_live_reload_file_monitor)
       config = socket.endpoint.config(:live_reload)
+      root = Path.expand(config[:root] || "")
 
       socket =
         socket
         |> assign(:patterns, config[:patterns] || [])
         |> assign(:debounce, config[:debounce] || 0)
-
+        |> assign(:root, root)
       {:ok, socket}
     else
       {:error, %{message: "live reload backend not running"}}
     end
   end
 
+  # HACK Backend tool for FileSystem (mac_listener or inotifywait) emits multiple events on a file modification.
+  # So we are throttling those events before sending assets_change frame.
+  @impl true
   def handle_info({:file_event, _pid, {path, _event}}, socket) do
-    %{patterns: patterns, debounce: debounce} = socket.assigns
+    with {:stale, socket} <- check_last_modified_at(socket, path) do
+      %{patterns: patterns, debounce: debounce, root: root} = socket.assigns
 
-    if matches_any_pattern?(path, patterns) do
-      ext = Path.extname(path)
+      if matches_any_pattern?(path, patterns) do
+        ext = Path.extname(path)
 
-      for {path, ext} <- [{path, ext} | debounce(debounce, [ext], patterns)] do
-        asset_type = remove_leading_dot(ext)
-        Logger.debug("Live reload: #{Path.relative_to_cwd(path)}")
-        push(socket, "assets_change", %{asset_type: asset_type})
+        for {path, ext} <- [{path, ext} | debounce(debounce, [ext], patterns)] do
+          asset_type = remove_leading_dot(ext)
+          Logger.debug("Live reload: #{Path.relative_to_cwd(path)}")
+          path = String.trim_leading(path, root)
+          push(socket, "assets_change", %{asset_type: asset_type, path: path})
+        end
       end
-    end
 
-    {:noreply, socket}
+      {:noreply, socket}
+    end
   end
 
   defp debounce(0, _exts, _patterns), do: []
@@ -72,4 +86,29 @@ defmodule Phoenix.LiveReloader.Channel do
 
   defp remove_leading_dot("." <> rest), do: rest
   defp remove_leading_dot(rest), do: rest
+
+  @become_stale_in 2
+  defp check_last_modified_at(socket, path) do
+    now = System.system_time(:second)
+    last_modified_at = socket.assigns[:last_modified_at][path] || 0
+
+    # Always save last_modified_at, even if already stale
+    socket = save_last_modified_at(socket, path, now)
+
+    if last_modified_at + @become_stale_in < now do
+      {:stale, socket}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  defp save_last_modified_at(socket, path, now) do
+    lma_map = Map.put(socket.assigns[:last_modified_at] || %{}, path, now)
+    assign(socket, :last_modified_at, lma_map)
+  end
+
+  @impl true
+  def terminate(reason, _socket) do
+    Logger.debug("Browser disconnected. Reason: #{inspect(reason)}")
+  end
 end
